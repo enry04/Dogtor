4d7f755a337f353a11f63b196ef7eab69a5b9b79
enry04 <enrico04v@gmail.com>
2023-02-05 19:08:07 +0100

    added logo

--- src/main-page/main-page.php
+++ src/main-page/main-page.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../common/css/page-style.css">
     <link rel="stylesheet" href="../common/css/header-style.css">
+    <link rel="icon" href="../common/images/page-logo.png">
+    
     <title>dogtor</title>
 </head>
 
