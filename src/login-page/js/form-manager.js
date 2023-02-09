721f3b836b6ef2054ee54bba90ce4845aa122b34
enry04 <enrico04v@gmail.com>
2023-02-09 13:17:02 +0100

    update

--- src/login-page/js/form-manager.js
+++ src/login-page/js/form-manager.js
@@ -1,30 +1,39 @@
 import FetchUtil from "../../common/js/fetch-util,js";
 
-
 class FormManager {
   constructor(parentElement) {
     this.rootElement = parentElement;
     this.elements = {};
-
   }
 
-  init(){
+  init() {
     this.initElements();
     this.initEventListeners();
   }
 
-  initElements(){
+  initElements() {
     this.elements = {
-        email: this.rootElement.querySelector(".email-text"),
-        username: this.rootElement.querySelector(".username-text"),
-        password: this.rootElement.querySelector(".password-text"),
-        form: this.rootElement.querySelector("submit-btn"),
-    }
+      email: this.rootElement.querySelector(".email-text"),
+      username: this.rootElement.querySelector(".username-text"),
+      password: this.rootElement.querySelector(".password-text"),
+      form: this.rootElement.querySelector("submit-btn"),
+    };
   }
 
-  initEventListeners(){
-    this.elements.form.addEventListener("submit",(event)=>{
-        event.preventDefault();
+  initEventListeners() {
+    this.elements.form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const formData = {
+        email: this.elements.email.value,
+        username: this.elements.username.value,
+        password: this.elements.password.value,
+      };
+      this.elements.email.value = "";
+      this.elements.username.value = "";
+      this.elements.password.value = "";
+      FetchUtil.postData("", formData).then((response) => {
+        console.log(response);
+      });
     });
   }
 }
