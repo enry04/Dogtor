41d1578c4b9c747bfe628ba2aca37d2b6985a626
enrico visentin <enrico04v@gmail.com>
2023-03-01 13:43:57 +0100

    update

--- src/doctor-page/php/read-visits.php
+++ src/doctor-page/php/read-visits.php
@@ -10,16 +10,16 @@ $data = json_decode($json);
 
 $filter = $data->filter;
 
-$query = $pdo->prepare("SELECT * FROM tprenotazione INNER JOIN tanimale ON tprenotazione.idAnimale = tanimale.id WHERE tprenotazione.stato = 'da effettuare' AND tprenotazione.motivazione LIKE CONCAT('%', :filter, '%') OR tprenotazione.descrizione LIKE CONCAT('%', :filter, '%') OR tprenotazione.nota LIKE CONCAT('%', :filter, '%') OR tprenotazione.gravita LIKE CONCAT('%', :filter, '%')");
+$query = $pdo->prepare("SELECT * FROM tprenotazione INNER JOIN tanimale ON tprenotazione.idAnimale = tanimale.id WHERE tprenotazione.stato != 'da confermare' AND tPrenotazione.stato != 'annullata' AND (tprenotazione.motivazione LIKE CONCAT('%', :filter, '%') OR tprenotazione.descrizione LIKE CONCAT('%', :filter, '%') OR tprenotazione.nota LIKE CONCAT('%', :filter, '%') OR tprenotazione.gravita LIKE CONCAT(:filter, '%') OR tprenotazione.data LIKE CONCAT('%', :filter, '%') OR tprenotazione.ora  LIKE CONCAT('%', :filter, '%')) ORDER BY data ASC, ora ASC");
 $query->execute(['filter' => $filter]);
 $visitData = $query->fetchAll();
 $result = null;
-if($visitData != null){
+if ($visitData != null) {
     $result = array(
         "data" => json_encode($visitData),
         "status" => "success",
     );
-}else {
+} else {
     $result = array(
         "data" => null,
         "status" => "error",
